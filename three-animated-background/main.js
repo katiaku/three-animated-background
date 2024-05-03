2ab85be67aa2a4002640664123f7dfd97e6755fc
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-03 14:01:37 +0200

    Add basic geometry

--- three-animated-background/main.js
+++ three-animated-background/main.js
@@ -1,13 +1,40 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    innerWidth / innerHeight, 
+    0.1, 
+    1000,
+);
+
 const renderer = new THREE.WebGLRenderer();
-
-console.log(scene);
-console.log(camera);
-console.log(renderer);
-
 renderer.setSize(innerWidth, innerHeight);
-
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(
+    5,
+    5,
+    10,
+    10,
+);
+const planeMaterial = new THREE.MeshBasicMaterial(
+    { 
+        color: 0xff0000,
+        side: THREE.DoubleSide,
+    },
+);
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    // planeMesh.rotation.x += 0.01;
+};
+
+animate();
