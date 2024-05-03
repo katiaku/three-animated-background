8ffee67c9dbca64d2ffe63a77a93d5b1ed3e80a4
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-03 13:33:40 +0200

    Set up scene, camera, renderer

--- three-animated-background/main.js
+++ three-animated-background/main.js
@@ -1,4 +1,13 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
 console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement);
